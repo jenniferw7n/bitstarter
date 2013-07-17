7c95d00626e74919c8753da96b4aca1c0c6751f6
Yun Zhan <jenniferw7n@gmail.com>
2013-07-17 22:34:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs');
 
   var buffer_index = fs.readFileSync(index.html);
   var response_txt = buf.toString( buffer_index );
