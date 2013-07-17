f29c7477cd0b58703e2ecfaa30338f311e4e36bb
Yun Zhan <jenniferw7n@gmail.com>
2013-07-17 23:09:40 +0000

    fixed bug in web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 var fs = require('fs');
- var response_txt =  fs.readFileSync("index.html").toString;
+ var response_txt =  fs.readFileSync("index.html").toString();
    response.send(response_txt);
 });
 
